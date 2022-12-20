37364a5c54c077a784233dcf8d7c76e7555a0f04
paul lovato <lovato.paul97@yahoo.com>
2022-12-20 18:06:21 -0500

    intern and engineer prompts created

--- index.js
+++ index.js
@@ -49,3 +49,71 @@ function showMenu(manager) {
     }
   });
 }
+
+function addEngineer(manager) {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Enter the engineer's name:"
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Enter the engineer's ID:"
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Enter the engineer's email:"
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: "Enter the engineer's GitHub username:"
+      }
+    ]).then(answers => {
+      // Create a new Engineer object using the answers
+      const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+      
+      // Add the engineer to the manager's team
+      manager.addTeamMember(engineer);
+      
+      // Go back to the menu
+      showMenu(manager);
+    });
+  }
+
+  function addIntern(manager) {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Enter the intern's name:"
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Enter the intern's ID:"
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Enter the intern's email:"
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: "Enter the intern's school:"
+      }
+    ]).then(answers => {
+      // Create a new Intern object using the answers
+      const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+      
+      // Add the intern to the manager's team
+      manager.addTeamMember(intern);
+      
+      // Go back to the menu
+      showMenu(manager);
+    });
+  }
